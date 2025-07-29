70f3430dd67b910003a5808e20a5dbccf004685a
devdrogba <aderogbasamueladeolu@gmail.com>
2025-07-29 12:39:37 +0100

    update

---
+++ swiper.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  slidesPerView:'auto',
+  spaceBetween:30,
+
+  // Navigation arrows
+  autoplay:{
+    delay: 2500,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  
+});
